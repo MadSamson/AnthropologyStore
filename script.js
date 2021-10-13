8bcd2d7b8494d4d42ffa5db549c8052083675a16
MadSamson <m.mahdi.samadi@gmail.com>
2021-10-13 20:49:28 +0200

    version 3

--- script.js
+++ script.js
@@ -1,53 +1,50 @@
 const url = "https://mock-data-api.firebaseio.com/webb21/products.json"
 let total = 0
+filterByRating()
 
-fetch(url)
-.then(response => response.json())
-.then(data => {
-    console.log(data)
-    renderdata (data)
-})
+function filterByRating(){
+    const plist = document.getElementById("filteredList")
+    plist.innerHTML = ""
 
-function renderdata(products) {
- 
-    products.forEach(product => {
-        const div = document.createElement("div")
-        const productName = document.createElement("p")
-        const productImage = document.createElement("img")
-        const productPrice = document.createElement("p")
-        const productRating = document.createElement("p")
-        const productStock = document.createElement("p")
-        const productDescription = document.createElement("p")
-        const buyButton = document.createElement("button")
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        data.filter(product=>{
+            if (product.rating>document.getElementById("rating").value) {
+                const div = document.createElement("div")
+                const productName = document.createElement("p")
+                const productImage = document.createElement("img")
+                const productInto = document.createElement("p")
+                const buyButton = document.createElement("button")
+            
+                productImage.src = product.images[0].src.small
+                productImage.alt = product.images[0].alt
+                productName.innerHTML = product.name
+                productInto.innerHTML = `${product.description} <br> Price: ${product.price} <br> Rating: ${product.rating} <br> Stock: ${product.stock}`
+                buyButton.innerText = "köp"
+                
+                div.appendChild(productName)
+                div.appendChild(productImage)
+                div.appendChild(productInto)
+                div.appendChild(buyButton)
+                plist.appendChild(div)
 
-        productImage.src = product.images[0].src.small
-        productImage.alt = product.images[0].alt
-        productName.innerHTML = product.name
-        productDescription.innerHTML = product.description
-        productPrice.innerHTML = `Price: ${product.price} `
-        productRating.innerHTML = `Rating: ${product.rating}`
-        productStock.innerHTML = `Stock: ${product.stock}`
-        buyButton.innerText = "köp"
+                // productImage.addEventListener("click", item =>{
+                //     totalPrice(product.price)
+                //     document.getElementById("total").innerHTML = `total: ${total}`
+                // })
         
-        div.appendChild(productName)
-        div.appendChild(productImage)
-        div.appendChild(productDescription)
-        div.appendChild(productPrice)
-        div.appendChild(productRating)
-        div.appendChild(productStock)
-        div.appendChild(buyButton)
-        document.body.appendChild(div)
-
-        buyButton.addEventListener("click", ()=>{
-            totalPrice(product.price)
-            addToCart(product.name, product.price)
+                buyButton.addEventListener("click", ()=>{
+                    totalPrice(product.price)
+                    addToCart(product.name, product.price)
+                })
+            }
         })
     })
 }
 
-
 function totalPrice(price){
-    console.log("clicked");
+    console.log("Clicked");
     total += price
     document.getElementById("total").innerHTML= `total: ${total}`
 }
